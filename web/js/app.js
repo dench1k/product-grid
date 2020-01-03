1250fafe0b85e5d2e447add1ce0526f064a002de
Den Moskvitin <denmoskvitin@gmail.com>
2020-01-03 23:15:49 +0200

    Clean from mocked data

--- web/js/app.js
+++ web/js/app.js
@@ -1,145 +1,18 @@
 "use strict";
-
-// var productGrid = function ($) {
-//   /**
-//    * get products from specific url and return an object with them
-//    * @param url
-//    * @param callback
-//    */
-//   function getProducts(url, callback) {
-//     var request = $.ajax({
-//       url: "".concat(url)
-//     }); // if everything is ok
-
-//     request.done(function (data) {
-//       var context = {
-//         products: data
-//       };
-//       console.log(context);
-
-//       if (typeof callback === 'function') {
-//         return callback(context);
-//       }
-
-//       return context;
-//     }); // if something went wrong
-
-//     request.fail(function (jqXHR, textStatus) {
-//       alert("Request failed: ".concat(textStatus));
-//     });
-//   }
-//   /**
-//    * get Handlebars template
-//    * @returns {*}
-//    */
-
-//   function getTemplate() {
-//     var source = document.getElementById('product-template').innerHTML;
-//     var template = Handlebars.compile(source);
-//     return template;
-//   }
-
-//   function renderTemplate(template, context) {
-//     return template(context);
-//   }
-
-//   function appendTemplate(data) {
-//     var template = getTemplate();
-//     var html = renderTemplate(template, data);
-//     $('.products').append(html);
-//   }
-//   /**
-//    * call functions on init
-//    */
-
-//   function init() {
-//     getProducts('http://localhost:3000/products', function (data) {
-//       appendTemplate(data);
-//     });
-//   }
-
-//   return {
-//     init: init
-//   };
-// }(jQuery);
-
-// productGrid.init();
-
-const productGridModule = (function ($) {
+const productGridModule = (function($) {
   //data
   const productsUrl = "http://localhost:3000/products/";
   let filterArr = [];
   let filteredByPriceData = null;
   let filteredByColorData = null;
-  let productsData = [
-    {
-      id: 0,
-      name: "Small Fresh Fish",
-      color: "ivory",
-      price: "43.00",
-      description: "desc",
-      image: {
-        src: "http://lorempixel.com/640/480/fashion"
-      }
-    },
-    {
-      id: 1,
-      name: "Tasty Frozen Chair",
-      color: "grey",
-      price: "455.00",
-      description: "desc",
-      image: {
-        src: "http://lorempixel.com/640/480/fashion"
-      }
-    },
-    {
-      id: 2,
-      name: "Intelligent Rubber Pizza",
-      color: "silver",
-      price: "484.00",
-      description: "desc",
-      image: {
-        src: "http://lorempixel.com/640/480/fashion"
-      }
-    },
-    {
-      id: 3,
-      name: "Handmade Granite Salad",
-      color: "purple",
-      price: "886.00",
-      description: "desc",
-      image: {
-        src: "http://lorempixel.com/640/480/fashion"
-      }
-    },
-    {
-      id: 4,
-      name: "Handmade Steel Mouse",
-      color: "salmon",
-      price: "387.00",
-      description: "desc",
-      image: {
-        src: "http://lorempixel.com/640/480/fashion"
-      }
-    },
-    {
-      id: 5,
-      name: "Incredible Granite Bike",
-      color: "lime",
-      price: "948.00",
-      description: "desc",
-      image: {
-        src: "http://lorempixel.com/640/480/fashion"
-      }
-    }
-  ];
+  let productsData = null;
 
   //cache DOM
   const $products = $(".js-products");
   const $productsTemplate = $("#product-template").html();
   const $colors = $(".js-filters__colors");
   const $colorsTemplate = $("#colors-template").html();
-  const $priceSelect = $('.js-filters__select');
+  const $priceSelect = $(".js-filters__select");
 
   //functions
   function renderProducts(productsArr) {
@@ -148,7 +21,7 @@ const productGridModule = (function ($) {
       products: productsArr
     };
     const result = template(data);
-    $products.html('');
+    $products.html("");
     $products.append(result);
   }
 
@@ -166,11 +39,11 @@ const productGridModule = (function ($) {
     let colorsArrAll = [];
     let colorsArrUnique = [];
 
-    data.forEach(function (element) {
+    data.forEach(function(element) {
       colorsArrAll.push(element.color);
     });
     console.log(colorsArrAll);
-    colorsArrUnique = colorsArrAll.filter(function (val, idx, arr) {
+    colorsArrUnique = colorsArrAll.filter(function(val, idx, arr) {
       return arr.indexOf(val) === idx;
     });
 
@@ -189,20 +62,19 @@ const productGridModule = (function ($) {
     return filterArr;
   }
 
-
   function getMatchedByColor(colorsArr) {
     let matchedArr = [];
     // if we have selected data by price use it instead of given productsData
     let data = /*filteredByPriceData ||*/ productsData;
     // show all products when filters are unchecked
     if (colorsArr.length === 0) {
-      console.log('kek');
+      console.log("kek");
       renderProducts(data);
       return;
     }
 
-    colorsArr.forEach(function (color) {
-      data.forEach(function (element) {
+    colorsArr.forEach(function(color) {
+      data.forEach(function(element) {
         if (color === element.color) {
           matchedArr.push(element.id);
         }
@@ -216,15 +88,15 @@ const productGridModule = (function ($) {
   function getFilteredProducts(filterIDArray) {
     let data = productsData;
     let filteredProductsArr = [];
-    filterIDArray.forEach(function (id) {
-      data.forEach(function (element) {
+    filterIDArray.forEach(function(id) {
+      data.forEach(function(element) {
         if (id === element.id) {
           filteredProductsArr.push(element);
         }
       });
     });
     filteredByColorData = filteredProductsArr;
-    let tempArr = filteredProductsArr.sort(function (a, b) {
+    let tempArr = filteredProductsArr.sort(function(a, b) {
       return a.id - b.id;
     });
     console.log(tempArr);
@@ -233,12 +105,16 @@ const productGridModule = (function ($) {
   }
 
   function filterByPrice() {
-    const NONE = 'NONE';
-    const ASC = 'ASC';
-    const DESC = 'DESC';
-    const filterSelectedOptionVal = $priceSelect.find('option[value]:selected').val();
+    const NONE = "NONE";
+    const ASC = "ASC";
+    const DESC = "DESC";
+    const filterSelectedOptionVal = $priceSelect
+      .find("option[value]:selected")
+      .val();
 
-    filteredByPriceData = Array.from(Object.create(/*filteredByColorData ||*/ productsData));
+    filteredByPriceData = Array.from(
+      Object.create(/*filteredByColorData ||*/ productsData)
+    );
 
     switch (filterSelectedOptionVal) {
       case NONE:
@@ -252,9 +128,8 @@ const productGridModule = (function ($) {
         filterDESC(filteredByPriceData);
         break;
       default:
-        console.error('Check option values for correct result');
+        console.error("Check option values for correct result");
     }
-
 
     function filterNONE() {
       filteredByPriceData = /*filteredByColorData ||*/ productsData;
@@ -263,7 +138,7 @@ const productGridModule = (function ($) {
     }
 
     function filterASC(data) {
-      data.sort(function (a, b) {
+      data.sort(function(a, b) {
         return a.price - b.price;
       });
       console.log(data);
@@ -271,34 +146,33 @@ const productGridModule = (function ($) {
     }
 
     function filterDESC(data) {
-      data.sort(function (a, b) {
+      data.sort(function(a, b) {
         return b.price - a.price;
       });
       console.log(data);
       renderProducts(data);
     }
-
   }
 
   function getData(url) {
     fetch(url)
-      .then(function (resp) {
+      .then(function(resp) {
         return resp.json();
       })
-      .then(function (data) {
+      .then(function(data) {
         productsData = data;
 
         renderProducts(productsData);
         renderColors(getColors(data));
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
   }
 
   //events
   $colors.on("change", "input[type=checkbox]", getFilterValues);
-  $priceSelect.on('change', filterByPrice);
+  $priceSelect.on("change", filterByPrice);
 
   function init() {
     getData(productsUrl);
@@ -310,7 +184,6 @@ const productGridModule = (function ($) {
 })(jQuery);
 
 productGridModule.init();
-
 
 // filter ->> render
 // render(filter)
