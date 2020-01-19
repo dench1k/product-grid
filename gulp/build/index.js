04933b72e1d351e428cdcbbe1132cf465c134ff8
Den Moskvitin <denmoskvitin@gmail.com>
2020-01-19 20:41:35 +0200

    Clean project to make it tiny

--- gulp/build/index.js
+++
@@ -1,5 +0,0 @@
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const { tasks } = require('./config');
-
-gulp.task('build', cb => runSequence(...tasks, cb));
--- gulp/serve/index.js
+++
@@ -1,5 +0,0 @@
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const { tasks } = require('./config');
-
-gulp.task('serve', cb => runSequence(...tasks, cb));
